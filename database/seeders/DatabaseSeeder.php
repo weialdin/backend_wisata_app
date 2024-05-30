e19b916ecbd8d773e2f970b3bdef44beaa4561c6
weialdin <whichaldin@gmail.com>
2024-05-30 21:23:16 +0700

    api auth, product, postman

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Category;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'adminwisata',
+            'email' => 'admin@wisata.com',
+            'password' => Hash::make('adminku1'),
         ]);
+
+        //category factory 2
+        Category::factory(2)->create();
+
+        //product factory 3
+        Product::factory(100)->create();
     }
 }
